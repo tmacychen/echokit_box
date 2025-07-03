a8060a57abece7945176841d08e7d5c0a41497a2
csh <458761603@qq.com>
2025-07-03 16:43:54 +0800

    fix: init lcd

--- src/ui.rs
+++ src/ui.rs
@@ -2,7 +2,7 @@ use embedded_graphics::{
     framebuffer::{buffer_size, Framebuffer},
     image::GetPixel,
     pixelcolor::{
-        raw::{LittleEndian, RawU16},
+        raw::{BigEndian, RawU16},
         Rgb565,
     },
     prelude::*,
@@ -13,25 +13,102 @@ use embedded_graphics::{
     },
 };
 use embedded_text::TextBox;
+use esp_idf_svc::sys::EspError;
 use u8g2_fonts::U8g2TextStyle;
 
-const GIF_IMG: &[u8] = include_bytes!("../assets/ht.gif");
+pub const GIF_IMG: &[u8] = include_bytes!("../assets/ht.gif");
 
 pub type ColorFormat = Rgb565;
 
-pub fn lcd_init() {
-    use esp_idf_svc::sys::hal_driver;
+const DISPLAY_WIDTH: usize = 240;
+const DISPLAY_HEIGHT: usize = 240;
+
+fn init_spi() -> Result<(), EspError> {
+    use esp_idf_svc::sys::*;
+    const GPIO_NUM_NC: i32 = -1;
+    const DISPLAY_MOSI_PIN: i32 = 47;
+    const DISPLAY_CLK_PIN: i32 = 21;
+    let mut buscfg = spi_bus_config_t::default();
+    buscfg.__bindgen_anon_1.mosi_io_num = DISPLAY_MOSI_PIN;
+    buscfg.__bindgen_anon_2.miso_io_num = GPIO_NUM_NC;
+    buscfg.sclk_io_num = DISPLAY_CLK_PIN;
+    buscfg.__bindgen_anon_3.quadwp_io_num = GPIO_NUM_NC;
+    buscfg.__bindgen_anon_4.quadhd_io_num = GPIO_NUM_NC;
+    buscfg.max_transfer_sz = (DISPLAY_WIDTH * DISPLAY_HEIGHT * std::mem::size_of::<u16>()) as i32;
+    esp!(unsafe {
+        spi_bus_initialize(
+            spi_host_device_t_SPI3_HOST,
+            &buscfg,
+            spi_common_dma_t_SPI_DMA_CH_AUTO,
+        )
+    })
+}
+
+static mut ESP_LCD_PANEL_HANDLE: esp_idf_svc::sys::esp_lcd_panel_handle_t = std::ptr::null_mut();
+
+fn init_lcd() -> Result<(), EspError> {
+    use esp_idf_svc::sys::*;
+    const DISPLAY_CS_PIN: i32 = 41;
+    const DISPLAY_DC_PIN: i32 = 40;
+    ::log::info!("Install panel IO");
+    let mut panel_io: esp_lcd_panel_io_handle_t = std::ptr::null_mut();
+    let mut io_config = esp_lcd_panel_io_spi_config_t::default();
+    io_config.cs_gpio_num = DISPLAY_CS_PIN;
+    io_config.dc_gpio_num = DISPLAY_DC_PIN;
+    io_config.spi_mode = 3;
+    io_config.pclk_hz = 40 * 1000 * 1000;
+    io_config.trans_queue_depth = 10;
+    io_config.lcd_cmd_bits = 8;
+    io_config.lcd_param_bits = 8;
+    esp!(unsafe {
+        esp_lcd_new_panel_io_spi(spi_host_device_t_SPI3_HOST as _, &io_config, &mut panel_io)
+    })?;
+
+    ::log::info!("Install LCD driver");
+    const DISPLAY_RST_PIN: i32 = 45;
+    let mut panel_config = esp_lcd_panel_dev_config_t::default();
+    let mut panel: esp_lcd_panel_handle_t = std::ptr::null_mut();
+
+    panel_config.reset_gpio_num = DISPLAY_RST_PIN;
+    panel_config.data_endian = lcd_rgb_data_endian_t_LCD_RGB_DATA_ENDIAN_BIG;
+    panel_config.__bindgen_anon_1.rgb_ele_order = lcd_rgb_element_order_t_LCD_RGB_ELEMENT_ORDER_RGB;
+    panel_config.bits_per_pixel = 16;
+
+    esp!(unsafe { esp_lcd_new_panel_st7789(panel_io, &panel_config, &mut panel) })?;
+    unsafe { ESP_LCD_PANEL_HANDLE = panel };
+
+    const DISPLAY_MIRROR_X: bool = false;
+    const DISPLAY_MIRROR_Y: bool = false;
+    const DISPLAY_SWAP_XY: bool = false;
+    const DISPLAY_INVERT_COLOR: bool = true;
+
+    ::log::info!("Reset LCD panel");
     unsafe {
-        let config: hal_driver::lcd_cfg_t = std::mem::zeroed();
-        hal_driver::lcd_init(config);
+        esp!(esp_lcd_panel_reset(panel))?;
+        esp!(esp_lcd_panel_init(panel))?;
+        esp!(esp_lcd_panel_invert_color(panel, DISPLAY_INVERT_COLOR))?;
+        esp!(esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY))?;
+        esp!(esp_lcd_panel_mirror(
+            panel,
+            DISPLAY_MIRROR_X,
+            DISPLAY_MIRROR_Y
+        ))?;
+        esp!(esp_lcd_panel_disp_on_off(panel, true))?; /* 启动屏幕 */
     }
+
+    Ok(())
+}
+
+pub fn lcd_init() -> Result<(), EspError> {
+    init_spi()?;
+    init_lcd()?;
+    Ok(())
 }
 
 pub fn flush_display(color_data: &[u8], x_start: i32, y_start: i32, x_end: i32, y_end: i32) -> i32 {
     unsafe {
-        let panel = std::mem::transmute(esp_idf_svc::sys::hal_driver::panel_handle);
         let e = esp_idf_svc::sys::esp_lcd_panel_draw_bitmap(
-            panel,
+            ESP_LCD_PANEL_HANDLE,
             x_start,
             y_start,
             x_end,
@@ -52,7 +129,7 @@ pub fn backgroud() -> Result<(), std::convert::Infallible> {
     let mut display = Box::new(Framebuffer::<
         ColorFormat,
         _,
-        LittleEndian,
+        BigEndian,
         DISPLAY_WIDTH,
         DISPLAY_HEIGHT,
         { buffer_size::<ColorFormat>(DISPLAY_WIDTH, DISPLAY_HEIGHT) },
@@ -166,7 +243,7 @@ pub struct UI {
         Framebuffer<
             ColorFormat,
             RawU16,
-            LittleEndian,
+            BigEndian,
             DISPLAY_WIDTH,
             DISPLAY_HEIGHT,
             { buffer_size::<ColorFormat>(DISPLAY_WIDTH, DISPLAY_HEIGHT) },
@@ -174,8 +251,6 @@ pub struct UI {
     >,
 }
 
-const DISPLAY_WIDTH: usize = 320;
-const DISPLAY_HEIGHT: usize = 240;
 const COLOR_WIDTH: u32 = 2;
 
 impl Default for UI {
@@ -183,7 +258,7 @@ impl Default for UI {
         let mut display = Box::new(Framebuffer::<
             ColorFormat,
             _,
-            LittleEndian,
+            BigEndian,
             DISPLAY_WIDTH,
             DISPLAY_HEIGHT,
             { buffer_size::<ColorFormat>(DISPLAY_WIDTH, DISPLAY_HEIGHT) },
@@ -292,7 +367,7 @@ impl UI {
         let mut display = Box::new(Framebuffer::<
             ColorFormat,
             _,
-            LittleEndian,
+            BigEndian,
             DISPLAY_WIDTH,
             DISPLAY_HEIGHT,
             { buffer_size::<ColorFormat>(DISPLAY_WIDTH, DISPLAY_HEIGHT) },
