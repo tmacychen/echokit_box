55a3f79201928a45c6edadd378dcd229fa847af2
zzz <458761603@qq.com>
2025-08-06 19:55:13 +0000

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 cd $HOME
+
+# install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
+# Initialize the ESP development environment
+# https://narukara.github.io/rust-on-esp-book-zh-cn
+
+# install espup
 # https://narukara.github.io/rust-on-esp-book-zh-cn/installation/riscv-and-xtensa.html
 cargo install --locked espup
 espup install
 
 cargo install --locked espflash
+
+# https://narukara.github.io/rust-on-esp-book-zh-cn/installation/std-requirements.html
 cargo install --locked ldproxy
-cargo install --locked esp-generate
+
+cargo install --locked cargo-generate
