f010a34254276cd5ecd39cd296303ff92b8211af
zzz <458761603@qq.com>
2025-08-06 11:06:17 +0000

    chore: add init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $HOME
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+# https://narukara.github.io/rust-on-esp-book-zh-cn/installation/riscv-and-xtensa.html
+cargo install --locked espup
+espup install
+
+cargo install --locked espflash
+cargo install --locked ldproxy
